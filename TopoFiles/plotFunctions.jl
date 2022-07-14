97d3a12f79814ea0937111924346827ab5f9476a
Sharon Kartika <ksharon8256@gmail.com>
2022-07-14 22:22:42 +0530

    created Plotfunctions

--- TopoFiles/plotFunctions.jl
+++ TopoFiles/plotFunctions.jl
@@ -5,49 +5,12 @@ using CairoMakie,
 
 include("scriptEvolutionGeneral.jl")
 
-function getGraph()
-
-    g = SimpleGraph(5)
-    add_edge!(g, 1, 2)
-    add_edge!(g, 2, 4)
-    add_edge!(g, 4, 3)
-    add_edge!(g, 3, 2)
-    add_edge!(g, 2, 5)
-    add_edge!(g, 5, 4)
-    add_edge!(g, 4, 1)
-    add_edge!(g, 1, 5)
-
-    # define some edge colors
-    edgecolors = [:black for i in 1:ne(g)]
-    edgecolors[4] = edgecolors[7] = :red
-
-    f, ax, p = GraphMakie.graphplot(g, layout=Shell(),
-        node_color=[:black, :red, :red, :red, :black],
-        edge_color=edgecolors)
-
-    hidedecorations!(ax)
-    hidespines!(ax)
-    ax.aspect = DataAspect()
-    return f
-end
-
-function graphPlotSelf()
-    g = complete_graph(3)
-    add_edge!(g, 1, 1)
-    add_edge!(g, 2, 2)
-    add_edge!(g, 3, 3)
-    f, ax, p = GraphMakie.graphplot(g)
-
-    hidedecorations!(ax)
-    hidespines!(ax)
-    ax.aspect = DataAspect()
-    return f
-end
 
 """Takes in a interaction matrix,
  and plots the network corresponding to it"""
 function plotNetwork(network)
     letterToInt(letter) = (Dict('A':'Z' .=> 1:26))[letter]
+    intToLetter(i) = (Dict(1:26 .=> 'A':'Z'))[i]
     mapcolors(i) = ((i==2) ? (:red) : (:green))
     g = SimpleDiGraph(size(network,1))
     
@@ -60,8 +23,12 @@ function plotNetwork(network)
     end
     
     edgeColors = mapcolors.(dfe.Type)
-    f, ax, p = GraphMakie.graphplot(g; arrow_size=0,
-        edge_color=edgeColors)
+    f, ax, p = GraphMakie.graphplot(g;
+        arrow_size=20,
+        arrow_shift=0.9,
+        edge_color=edgeColors,
+        nlabels=string.(intToLetter.(1:2)))
+
     hidedecorations!(ax); hidespines!(ax); ax.aspect = DataAspect()
     return f
 end
